5b33c1d0c0abd03fee94867fad2d03cf23cee9cb
jf-gbg <malmmlam@outlook.com>
2022-07-30 15:41:50 +0200

    update program to work with phrases

--- main.cpp
+++ main.cpp
@@ -5,19 +5,20 @@ using std::cin;
 using std::string;
 
 int main() {
-    string word1, word2;
+    string phrase1;
+    string phrase2;
 
-    cout << "Please enter a word: " << '\n';
-    cin >> word1;
-    cout << "Please enter a second word: " << '\n';
-    cin >> word2;
+    cout << "Please enter a phrase: " << '\n';
+    getline(cin, phrase1);
+    cout << "Please enter a second phrase: " << '\n';
+    getline(cin, phrase2);
 
-    if (word1.length() > word2.length()) {
-        cout << "The first word was longer" << '\n';
-    } else if (word1.length() < word2.length()) {
-        cout << "The second word was longer" << '\n';
-    } else if (word1.length() == word2.length()) {
-        cout << "Both words are the same length" << '\n';
+    if (phrase1.length() > phrase2.length()) {
+        cout << "The first phrase was longer" << '\n';
+    } else if (phrase1.length() < phrase2.length()) {
+        cout << "The second phrase was longer" << '\n';
+    } else if (phrase1.length() == phrase2.length()) {
+        cout << "Both phrases are the same length" << '\n';
     }
 
     return 0;
